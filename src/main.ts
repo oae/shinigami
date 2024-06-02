3e5456f20a46a20e6061e6ecebe07a5fb41fb565
Alperen Elhan <alperen@elhan.io>
2024-06-02 21:35:32 +0300

    fix: allow locahost:3000 origin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 const port = 3001;
 
@@ -14,6 +15,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    // add multiple origins here
+    origin: ['http://localhost:3000/'],
+  });
 
   await app.listen(port);
 }
